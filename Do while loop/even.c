2a58702e56881e15247486a8abae732d59f4aa84
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-11-22 15:25:56 +0530

    Update even.c

--- Do while loop/even.c
+++ Do while loop/even.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-
 main()
 {
-//print even numbers using do while loop
-    int n;
-    printf("enter the value of n:");
-    scanf("%d",&n);
-    do
-    {
-        if(n%2==0)
-        {
-            printf("%d ",n);
-        }
-    } while (n>=1);
+	int n;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	do
+	{
+		if(n%2==0)
+		{
+			printf("%d ",n);
+		}
+		n--;
+	}while(n>=1);
+}
