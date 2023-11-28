2822f5f619c5ef92878728895df50f00810bef9b
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-11-28 15:59:49 +0530

    Add files via upload

---
+++ pattern/15.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+	int i,j;
+	   
+//1
+//0 1
+//1 0 1
+//0 1 0 1
+//1 0 1 0 1	   
+
+   for(i=1;i<=5;i++)
+   {
+   	for(j=i;j>=1;j--)
+   	{
+   		if(j%2==0)
+   		{
+   			printf("0 ");
+		   }
+		   else
+		   {
+		   	printf("1 ");
+		   }
+	   }
+	   printf("\n");
+   }
+}
