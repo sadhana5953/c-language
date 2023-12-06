b4febc4e4a993436f26d57deb4192802ac7db68f
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-06 21:13:21 +0530

    Add files via upload

---
+++ 1d array2/acending.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	int n,i,j,x;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	int a[n];
+	
+	for(i=0;i<n;i++)
+	{
+		printf("enter the value of a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				x=a[i];
+				a[i]=a[j];
+				a[j]=x;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
