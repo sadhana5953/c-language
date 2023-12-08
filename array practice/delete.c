5aca6cea9dbcdafd230ed320c62915a13ddee6da
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-08 21:02:51 +0530

    Add files via upload

---
+++ array practice/delete.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+    int n,x;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    int a[n],i;
+
+    for(i=0; i<n; i++)
+    {
+        printf("enter the value a[%d]:",i);
+        scanf("%d",&a[i]);
+    }
+    printf("enter the position:");
+    scanf("%d",&i);
+    a[i]=x;
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
