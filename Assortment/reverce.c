9b23ffb7d995682d7a5da0c5511eac76171e033c
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-12 00:59:06 +0530

    Update reverce.c

--- Assortment/reverce.c
+++ Assortment/reverce.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int n,x;
+	printf("enter the size of array:");
+	scanf("%d",&n);
+	int a[n],i,j;
+	for(i=0;i<n;i++)
+	{
+		printf("enter a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+ 	i=0;
+	for(j=n-1;i<j;j--)
+	{
+		x=a[i];
+		a[i]=a[j];
+		a[j]=x;
+		i++;
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]:%d\n",i,a[i]);
+	}
+}
 
