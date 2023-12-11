b6aa9c7eb9fb15a62b4c53d9d43565d8a6c30b24
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-11 20:51:59 +0530

    Add files via upload

---
+++ Assortment/anti diagonal.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+{
+	//9. Wap to find sum of diagonal element in 2d array.
+	int x,y,sum=0;
+	printf("enter the size of array:");
+	scanf("%d",&x);
+	printf("enter the size of array:");
+	scanf("%d",&y);
+	
+	int a[x][y],i,j;
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+	{
+		printf("enter a[%d][%d]:",i,j);
+		scanf("%d",&a[i][j]);
+	}
+	}
+	j=2;
+	for(i=0;i<3;i++)
+	{
+		sum=sum+a[i][j];
+		j--;
+	}
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+	{
+		printf("%d ",a[i][j]);
+	}
+	printf("\n");
+	}
+	printf("sum:%d",sum);
+	
+	
+}
