99e408ae19de166eb296a28fa2a0db7e7f011f6c
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-12 20:57:08 +0530

    Update frequency.c

--- Assortment/frequency.c
+++ Assortment/frequency.c
@@ -1,1 +1,34 @@
-
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter the size of array:");
+	scanf("%d",&n);
+	
+	int a[n],b[n];
+	int i,j,x=1;
+	
+	for(i=0;i<n;i++)
+	{
+		printf("enter a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				x++;
+				a[j]=0;
+			}
+		}
+		b[i]=x;
+		if(a[i]!=0)
+		{
+			printf("%d -> %d\n",a[i],b[i]);
+		}
+		x=1;
+	}
+}
