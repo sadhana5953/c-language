3fb790b1730ce4462dd00cce4df338718d5a68b5
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-13 15:53:09 +0530

    Update title.c

--- string/title.c
+++ string/title.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 main()
 {
-	char a[100];
+	char a[50];
 	printf("enter your name:");
 	int i;
 	for(i=0;i<100;i++)
 	{
 		scanf("%c",&a[i]);
-		if(a[i-1]==' '||i==0)
+		if(a[i-1]==' '&&a[i]>=97&&a[i]<=122||i==0&&a[i]>=97&&a[i]<=122)
 		{
 			printf("%c",a[i]-32);
 		}
+		else if(i!=0&&a[i-1]!=' '&&a[i]>=65&&a[i]<=90)
+		{
+			printf("%c",a[i]+32);
+		}
 		else
 		{
-			printf("%c",a[i]);
+			printf("%c",a[i]); 
 		}
 	}
 }
