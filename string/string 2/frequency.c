d51b6da54dd148d4bd7ec8335f086adc47939e37
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-13 23:26:38 +0530

    Add files via upload

---
+++ string/string 2/frequency.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	char a[50];
+	printf("enter your name:");
+	scanf("%s",&a);
+	
+	int i,j,x=1,length=0;
+	for(i=0;a[i]!=0;i++)
+	{
+		length++;
+	}
+	int b[length];
+	for(i=0;i<length;i++)
+	{
+		for(j=i+1;j<length;j++)
+		{
+			if(a[i]==a[j])
+			{
+				a[j]=0;
+				x++;
+			}
+		}
+		b[i]=x;
+		if(a[i]!=0)
+		{
+			printf("%c -> %d\n",a[i],b[i]);
+		}
+		x=1;
+	}
+	
+}
