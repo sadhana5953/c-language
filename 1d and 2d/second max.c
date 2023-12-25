6c7824ddd2f8d5724cd9292d0d33f152ca3aa8e6
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2023-12-25 18:26:02 +0530

    Update second max.c

--- 1d and 2d/second max.c
+++ 1d and 2d/second max.c
@@ -1,32 +1,34 @@
 #include<stdio.h>
 main()
 {
-	int n;
-	printf("enter the value of n:");
-	scanf("%d",&n);
-	
-	int a[n],i,j;
-	
-	for(i=0;i<n;i++)
-	{
-		printf("enter a[%d]:",i);
-		scanf("%d",&a[i]);
-	}
-	int max=a[0];
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]>a[j])
-		{
-			max=a[i];
-			a[i]=a[j];
-			a[j]=max;
-		}
-		}
-	}
-	max=a[n-2];
-	
-		printf("second maximum number is : %d ",max);
-		
+    int n;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+    int a[n];
+
+    int i;
+    for(i=0; i<n; i++)
+    {
+        printf("enter a[%d]:",i);
+        scanf("%d",&a[i]);
+    }
+    int max=a[0];
+    int second_max=0;
+    for(i=0; i<n; i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        if(a[i]>second_max&&a[i]<max)
+        {
+            second_max=a[i];
+        }
+    }
+    printf("max:%d\n",max);
+    printf("second max:%d", second_max);
+
 }
