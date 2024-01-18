a2d8be5324ecc1969b6efa84892053887da8f5e6
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2024-01-18 10:08:42 +0530

    Update 2d cube .c

--- pointer/pointer 2/2d cube .c
+++ pointer/pointer 2/2d cube .c
@@ -1,43 +1,85 @@
 #include<stdio.h>
+
 void cube(int *a,int n)
+
 {
+
 	int i,j;
 
+
+
 	for(i=0;i<n;i++)
+
 	{
+
 	
+
 		for(j=0;j<n;j++)
+
 		{
+
 			*a=*a**a**a;
+
 			*a++;
+
 		}
+
 	}
+
 }
+
 main()
+
 {
+
 	// Write a Program to find cubes of all elements from a given 2D array using Pointer with UDF.
+
 	int n,i,j;
+
 	printf("enter the size of array :");
+
 	scanf("%d",&n);
+
 	
+
 	int a[n][n];
+
 	
+
 	for(i=0;i<n;i++)
+
 	{
+
 		for(j=0;j<n;j++)
+
 		{
+
 			printf("enter a[%d][%d] :",i,j);
+
 		    scanf("%d",&a[i][j]);
+
 		}
+
 	}
+
 	
-	cube(&a,n);
+
+	cube(a,n);
+
 	
+
 	for(i=0;i<n;i++)
+
 	{
+
 		for(j=0;j<n;j++)
+
 		{
+
 		    printf("%d ",a[i][j]);
+
 		}
+
 	}
+
 }
